9b705b7a5ef32c5d1f2436802d68f7939c14e38f
Johnson Adodo <johnsonadodo@gmail.com>
2021-03-23 09:01:47 +0100

    added page for global styles

---
+++ src/globalStyles.js
@@ -0,0 +1,10 @@
+// All style rules go here in different exported objects ..
+
+const primaryColor = "darkblue";
+
+export const exampleStyle = {
+  height: 300,
+  backgroundColor: primaryColor,
+  // hence we just import {exampleStyle} from 'globalStyles'
+  // sorry for being so obvious
+};
